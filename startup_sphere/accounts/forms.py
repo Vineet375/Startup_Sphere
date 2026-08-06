53d14cf75ffbc446f18e31ca453bc2378aa82864
Vineet375 <175447242+Vineet375@users.noreply.github.com>
2026-08-06 18:01:38 +0530

    UI Fixes: Favicon, responsive scroll fix, admin removal, professional landing page

--- startup_sphere/accounts/forms.py
+++ startup_sphere/accounts/forms.py
@@ -3,11 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from core.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    # Override role choices to exclude 'admin'
+    ROLE_CHOICES = (
+        ('founder', 'Founder'),
+        ('mentor', 'Mentor'),
+        ('investor', 'Investor'),
+        ('applicant', 'Applicant'),
+    )
+    role = forms.ChoiceField(choices=ROLE_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'role')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
+        for field_name, field in self.fields.items():
+            if field_name != 'role':
+                field.widget.attrs.update({'class': 'form-control'})
