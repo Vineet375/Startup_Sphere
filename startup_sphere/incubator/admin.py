8eca6bd0a8a0b1fad81816128dd292bc0bc1e09a
Vineet375 <175447242+Vineet375@users.noreply.github.com>
2026-08-06 18:26:11 +0530

    Admin updates and loading animation

--- startup_sphere/incubator/admin.py
+++ startup_sphere/incubator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Startup
 
-# Register your models here.
+@admin.register(Startup)
+class StartupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'founder', 'industry', 'stage', 'created_at')
+    list_filter = ('industry', 'stage', 'created_at')
+    search_fields = ('name', 'founder__username', 'founder__email', 'description')
